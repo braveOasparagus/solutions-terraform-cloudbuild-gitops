33bb9900eff7f0ae94e5f6ff1889c9aae46a4698
sungwon <superoer@gmail.com>
2022-12-11 22:54:41 +0900

    change things

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "sungwon-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "-int-200421-tfstate"
     prefix = "env/prod"
   }
 }
